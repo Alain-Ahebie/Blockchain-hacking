05dfb79e01ea3ef40a18aed57ace83e297b2b393
alainahebie <alainahebie@gmail.com>
2024-01-18 00:24:09 +0100

    ADD COMMENT TO SIGNING TRANSACTIONS

--- PART_1/04_Signing_transactions.py
+++ PART_1/04_Signing_transactions.py
@@ -1,6 +1,6 @@
 from web3 import Web3
 
-# Make a connection
+# Make local connection 
 GANACHE = 'http://127.0.0.1:7545'
 web3 = Web3(Web3.HTTPProvider(GANACHE))
 print(f'Connected: {web3.is_connected()}')
@@ -17,12 +17,14 @@ transaction = {
      'to': second_account,
      'value' : web3.to_wei(1, 'ether'),
      'gas' : 3000000,
-     'gasPrice' : web3.to_wei('20', 'gwei')
+     'gasPrice' : web3.to_wei('100000', 'gwei')
      
 }
-#sign and send transaction (transaction + private key)
+# Fund for gas = gas * price + value 
+# sign and send transaction (transaction + private key)
 signed_transacion = web3.eth.account.sign_transaction(transaction, first_account_priv)
 transaction_hash = web3.eth.send_raw_transaction(signed_transacion.rawTransaction)
 
+#print transaction Detail
 print("HASH",Web3.to_hex(transaction_hash))
 print(web3.eth.get_transaction(transaction_hash))
